1ad3961ae26ae391470ccb39f7789fdf749e8916
Celthim <34304819+Celthim@users.noreply.github.com>
2021-01-31 15:22:27 +0100

    configurable wall OK

--- GPE/Walls/configurable_wall.gd
+++ GPE/Walls/configurable_wall.gd
@@ -1,16 +1,35 @@
 extends Spatial
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+enum wall_type{
+	Phantom,
+	Regular,
+	Persistent,
+	Fake,
+	Shadow
+}
 
-export var 
+export (wall_type) var wt = wall_type.Regular;
+export var hasCollision = false;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	var wallMaterial; 
+	match wt:
+		wall_type.Phantom:
+			wallMaterial = load("res://Assets/Materials/floor_phantom.tres")
+		wall_type.Regular:
+			wallMaterial = load("res://Assets/Materials/floor_regular.tres")
+		wall_type.Persistent:
+			wallMaterial = load("res://Assets/Materials/floor_persistent.tres")
+		wall_type.Fake:
+			wallMaterial = load("res://Assets/Materials/floor_fake.tres")
+		wall_type.Shadow:
+			wallMaterial = load("res://Assets/Materials/floor_shadow.tres")
+			
+	$wall.material_override = wallMaterial;
+	
+	$wall/StaticBody/CollisionShape.disabled = !hasCollision;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
